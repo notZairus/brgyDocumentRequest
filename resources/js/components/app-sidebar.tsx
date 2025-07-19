98282e4fa12557d51d2740e53b1d13da3c21d8e3
Zairus Bermillo <166342742+notZairus@users.noreply.github.com>
2025-07-19 11:49:18 +0800

    added new user tab on sidebar

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -4,7 +4,7 @@ import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid, ShieldAlert } from 'lucide-react';
+import { CircleUserRound, LayoutGrid, ShieldAlert } from 'lucide-react';
 import AppLogo from './app-logo';
 
 
@@ -22,6 +22,12 @@ const mainNavItems: NavItem[] = [
         icon: ShieldAlert,
         for: "admin"
     },
+    {
+        title: 'Users',
+        href: '/users',
+        icon: CircleUserRound,
+        for: "admin"
+    }
 ];
 
 const footerNavItems: NavItem[] = [
